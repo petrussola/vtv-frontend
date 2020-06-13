75ad41527b6ab3e7e4d2d01b2d781d2faf0864c3
Pere <pere.solaclaver@gmail.com>
2020-06-14 00:02:31 +0100

    Display buttons at the end of the quizz

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const startButton = document.getElementById('start-button');
 const errorContainer = document.getElementById('error');
 const pregunta = document.getElementById('pregunta');
 const respostes = document.getElementById('respostes');
-const score = document.getElementById('score');
+// const aux = document.getElementById('aux');
 const nextStep = document.getElementById('next-step');
 
 const endpoint = 'https://vtv-vila-server.herokuapp.com/test';
@@ -22,7 +22,7 @@ const state = {
 		},
 	],
 	fetchedQuestions: false,
-	questionCounter: 0,
+	questionCounter: 9,
 	score: 0,
 	socialMediaText:
 		'Soc un VTV (Vilafranqui de Tota la Vida)! Vols saber si ho ets? Ves a www.xyz.com i respon el questionari i, qui sap, potser guanyes una medalla!',
@@ -49,55 +49,59 @@ const fetchQuestions = async () => {
 // display question in #content node
 function displayNextQuestion() {
 	// change text of pregunta to whatever pregunta we are asking, which is determined by the questioncounter
-	pregunta.textContent = `${state.questions[state.questionCounter].pregunta}`;
+	content.innerHTML = `<h1>${
+		state.questions[state.questionCounter].pregunta
+	}</h1>`;
+	// pregunta.textContent = `${state.questions[state.questionCounter].pregunta}`;
 	// display available answers
+	const respostes = document.createElement('div');
+	respostes.id = 'respostes';
 	respostes.innerHTML = state.questions[state.questionCounter].respostes
 		.map((item) => {
-			return `<button>${item}</button>`;
+			return `<button id="answer-option">${item}</button>`;
 		})
 		.join('');
+	content.appendChild(respostes);
 	// display up to date score
-	score.textContent = `Puntuació: ${state.score} / ${
-		state.questions.length - 1
-	}`;
+	const aux = document.createElement('div');
+	aux.id = 'aux';
+	const questionsLeft = state.questions.length - state.questionCounter;
+	aux.textContent = `${questionsLeft === 1 ? 'Falta' : 'Falten'} ${
+		state.questions.length - state.questionCounter
+	} ${questionsLeft === 1 ? 'pregunta' : 'preguntes'}.`;
+	content.appendChild(aux);
 }
 
 function displayResults() {
 	let congratulation;
 	let explanation;
 	let action;
+	// score === 9 or 10
 	if (state.score >= 9) {
-		console.log('9 - 10');
 		congratulation = 'Enhorabona';
 		explanation =
 			"Comparteix la teva puntuacio amb els teus amics i descarrega't la medalla";
 		action = 'Comparteix la teva puntuació';
 	} else if (state.score <= 8 && state.score >= 5) {
-		console.log('5 - 8');
 		congratulation = 'Hmm';
 		explanation =
 			"Si bé has demostrat un bon coneixement de la teva vila, no estàs encara a la altura per guanyar-te la medalla. Torna'ho a provar.";
 		action = 'Torna-ho a provar';
 	} else {
-		console.log('suspes');
 		congratulation = 'Em sap greu';
 		explanation =
 			"Has d'estudiar més! Quan estiguis preparat torna'ho a provar.";
 		action = 'Torna-ho a provar';
 	}
-	respostes.innerHTML = '';
-	pregunta.textContent = `${congratulation}, has encertat ${state.score} preguntes. ${action}`;
-	score.innerHTML = '';
-	nextStep.style.visibility = 'visible';
+	content.innerHTML = `<h1>${congratulation}, has encertat ${state.score} preguntes. ${action}</h1>`;
+	const aux = document.createElement('button');
 	if (action === 'Torna-ho a provar') {
-		nextStep.innerHTML = `<button>${action}</button>`;
+		aux.id = 'tryAgain';
+		aux.textContent = `${action}`;
 	} else {
-		console.log('twitter');
-		nextStep.innerHTML = `<div>
-		<a class="twitter-share-button" href="https://twitter.com/intent/tweet?text=${state.socialMediaText} data-size="large">Comparteix a Twitter</a>
-		
-		</div>`;
+		aux.innerHTML = `<a class="twitter-share-button" href="https://twitter.com/intent/tweet?text=${state.socialMediaText} data-size="large">Comparteix a Twitter</a>`;
 	}
+	content.appendChild(aux);
 }
 
 // reset state for those who have to restart the quizz
@@ -112,10 +116,18 @@ function reset() {
 function displayInitialQuestion() {
 	// pregunta.textContent = `${state.questions[state.questionCounter].pregunta}`;
 	// button to start to be visible
-	startButton.style.visibility = 'visible';
-	startButton.textContent = 'Comencar el test';
+	content.innerHTML = `<button id='start-button'>Començar el test</button>`;
+	// startButton.style.visibility = 'visible';
+	// startButton.textContent = 'Comencar el test';
 	// hide next step section just in case user is retaking the quizz
-	nextStep.style.visibility = 'hidden';
+	// nextStep.style.visibility = 'hidden';
+}
+
+function isLastQuestion() {
+	if (state.questionCounter === state.questions.length) {
+		return true;
+	}
+	return false;
 }
 
 ////////////
@@ -131,49 +143,76 @@ window.onload = () => {
 };
 
 // click button to start test
-startButton.addEventListener('click', () => {
+content.addEventListener('click', (e) => {
+	const targetId = e.target.id;
+	console.log(`fired ${targetId}`);
+	switch (targetId) {
+		// user has clicked on start button
+		case 'start-button':
+			state.questionCounter++;
+			displayNextQuestion();
+			break;
+		// user has clicked on answer
+		case 'answer-option':
+			const selectedAnswer = e.target.textContent;
+			if (selectedAnswer === state.questions[state.questionCounter].correcte) {
+				state.score++;
+			}
+			state.questionCounter++;
+			const isLast = isLastQuestion();
+			if (isLast) {
+				displayResults();
+			} else {
+				displayNextQuestion();
+			}
+			break;
+		case 'tryAgain':
+			console.log('aux');
+			reset();
+			break;
+	}
 	// clear error message when clicking on start button after test has been triggered
-	if (errorContainer.className.includes('activated')) {
-		errorContainer.classList.remove('activated');
-		errorContainer.textContent = '';
-	}
+	// if (errorContainer.className.includes('activated')) {
+	// 	errorContainer.classList.remove('activated');
+	// 	errorContainer.textContent = '';
+	// }
 	// hide button start test first time we click on the button
-	if (state.questionCounter === 0) {
-		startButton.style.visibility = 'hidden';
-	}
+	// if (state.questionCounter === 0) {
+	// 	startButton.style.visibility = 'hidden';
+	// }
 	// increase count of question
-	state.questionCounter++;
-	displayNextQuestion();
+	// state.questionCounter++;
+	// displayNextQuestion();
 });
 
 // when a user clicks on the answer
-respostes.addEventListener('click', (e) => {
-	const resposta = e.target.textContent;
-	if (e.target.nodeName === 'BUTTON') {
-		if (resposta === state.questions[state.questionCounter].correcte) {
-			console.log('lool');
-			state.score++;
-			displayNextQuestion();
-		}
-		if (state.questionCounter === state.questions.length - 1) {
-			displayResults();
-		} else {
-			// increase count of question
-			state.questionCounter++;
-			displayNextQuestion();
-		}
-	}
-});
+// respostes.addEventListener('click', (e) => {
+// 	const resposta = e.target.textContent;
+// 	if (e.target.nodeName === 'BUTTON') {
+// 		if (resposta === state.questions[state.questionCounter].correcte) {
+// 			console.log('lool');
+// 			state.score++;
+// 			displayNextQuestion();
+// 		}
+// 		if (state.questionCounter === state.questions.length - 1) {
+// 			displayResults();
+// 		} else {
+// 			// increase count of question
+// 			state.questionCounter++;
+// 			displayNextQuestion();
+// 		}
+// 	}
+// });
 
-// click on action button at the end of the quizz
-nextStep.addEventListener('click', (e) => {
-	console.log(e.target.textContent);
-	switch (e.target.textContent) {
-		case 'Torna-ho a provar':
-			reset();
-			break;
-		case 'Comparteix a Twitter':
-			console.log('twitter');
-			break;
-	}
-});
+// // click on action button at the end of the quizz
+// nextStep.addEventListener('click', (e) => {
+// 	console.log(e.target.textContent);
+// 	switch (e.target.textContent) {
+// 		case 'Torna-ho a provar':
+// 			reset();
+// 			break;
+// 		case 'Comparteix a Twitter':
+// 			console.log('twitter');
+// 			break;
+// 	}
+// });
